d3b3f6736ba72508fdd749bd83d24a75f2d21ebd
ivanandresmadriz <madrizivanandres@gmail.com>
2025-07-25 20:10:40 -0500

    Add WKND coming soon animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
       </h1>
 
       {/* ✅ New subtitle */}
-      <p className="waitlist-subtitle fade-out">
+      <p className="waitlist-subtitle">
   Drop your email and be the first to know when WKND launches.
 </p>
 
