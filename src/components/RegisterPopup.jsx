11a97ef57904ec4a434c6a6fd78ab96abbc78796
ivanandresmadriz <madrizivanandres@gmail.com>
2025-10-20 15:58:01 -0500

    vfev

--- src/components/RegisterPopup.jsx
+++ src/components/RegisterPopup.jsx
@@ -574,7 +574,6 @@ export default function RegisterPopup({
             )}
           </div>
         )}
-
         <div className="pay-buttons">
           <button
             className="buy-button"
